b48f8e8f19ff749d346ecda62d295786f04cf113
Paramonov Dmitry <113761691+NoNeedToBother@users.noreply.github.com>
2023-09-29 00:25:24 +0300

    added version 1.2

---
+++ V1.2__createTables.sql
@@ -0,0 +1,26 @@
+create table Sticker (
+	id bigint
+	name varchar
+	author varchar
+	shortName varchar
+	file varchar
+);
+
+create table MediaChannel (
+	id bigint
+	name varchar
+	description varchar
+	accessLevel varchar
+);
+
+create table Bot (
+	id bigint
+	name varchar
+	avatar varchar
+	description varchar
+	link varchar
+);
+
+create table PersonalChannel (
+	channelId bigint
+);
