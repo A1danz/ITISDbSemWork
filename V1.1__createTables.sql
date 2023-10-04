b25a0ca623ee77fcba331c1f6bc586544ae1b2d1
lldan <safinildan@gmail.com>
2023-10-04 21:52:10 +0300

    фикс ошибки

--- V1.1__createTables.sql
+++ V1.1__createTables.sql
@@ -1,31 +1,31 @@
-CREATE TABLE Role (
+create table Role (
   Name varchar,
   Description text,
   Avatar varchar,
-)
-
-CREATE TABLE Channel (
-    Id int,
-    ServerName varchar,
-    ServerMembers int,
-    ServerChannels varchar,
-    ServerAvatar varchar
 );
 
-CREATE TABLE Message (
-    Id int,
-    Text varchar,
-    Attachments varchar,
-    Sender varchar,
-    TimeSent timestamp,
-    ChannelId int
+create table Channel (
+  Id int,
+  ServerName varchar,
+  ServerMembers int,
+  ServerChannels varchar,
+  ServerAvatar varchar
 );
 
-CREATE TABLE Activity (
-    Id int,
-    Name varchar,
-    Genre varchar,
-    Description text,
-    WebAppLink varchar,
-    Region varchar
+create table Message (
+  Id int,
+  Text varchar,
+  Attachments varchar,
+  Sender varchar,
+  TimeSent timestamp,
+  ChannelId int
+);
+
+create table Activity (
+  Id int,
+  Name varchar,
+  Genre varchar,
+  Description text,
+  WebAppLink varchar,
+  Region varchar
 );
