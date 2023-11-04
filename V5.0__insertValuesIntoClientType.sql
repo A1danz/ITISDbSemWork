77834f8c1884c541a2a0be40e4751394fa870b62
Paramonov Dmitry <113761691+NoNeedToBother@users.noreply.github.com>
2023-11-04 11:44:44 +0300

    more suitable values for client types

--- V5.0__insertValuesIntoClientType.sql
+++ V5.0__insertValuesIntoClientType.sql
@@ -1,8 +1,4 @@
-insert into ClientType (name) VALUES ('Windows');
-insert into ClientType (name) VALUES ('Linux');
-insert into ClientType (name) VALUES ('macOS');
-insert into ClientType (name) VALUES ('Android');
-insert into ClientType (name) VALUES ('iOS');
+insert into ClientType (name) VALUES ('PC');
+insert into ClientType (name) VALUES ('Mobile');
 insert into ClientType (name) VALUES ('Browser');
-insert into ClientType (name) VALUES ('Xbox');
-insert into ClientType (name) VALUES ('PlayStation');
+insert into ClientType (name) VALUES ('Console');
